07ac5a51ec734545a26db8d9f872c11ef7846e89
Joseph Pierre-Louis <97973691+JoeyPL5@users.noreply.github.com>
2026-03-28 17:00:10 -0400

    scratch sounds

--- TriGameDevMontlyGameJam5/destruction-jam-game/Scripts/scene_loader.gd
+++ TriGameDevMontlyGameJam5/destruction-jam-game/Scripts/scene_loader.gd
@@ -1,7 +1,31 @@
 extends CanvasLayer
 
+@export var bg : Panel
+@export var transition_duration : float = Animations.DEFAULT_ENTRANCE_ANIMATION_DUR
+
+
+func _ready() -> void:
+	bg.position = Vector2(0, -bg.size.y)
+	change_scene("res://Scenes/Menus/MainMenuUI.tscn")
+
 
 func change_scene(scene_file : String) -> void:
-	self.scene_to_load = scene_file
-	self.visible = true
 	ResourceLoader.load_threaded_request(scene_file)
+	var offscreen_position : Vector2 = Vector2(0, -bg.size.y)
+	Animations.tween_position(bg, offscreen_position, Vector2.ZERO - offscreen_position, transition_duration)
+	await thread_load_scene(scene_file)
+	get_tree().change_scene_to_file(scene_file)
+	Animations.tween_position(bg, Vector2.ZERO, (offscreen_position * Vector2(1, -1)) - Vector2.ZERO, transition_duration)
+	
+	
+func thread_load_scene(scene_file : String) -> void:
+	var thread_status : int = ResourceLoader.load_threaded_get_status(scene_file)
+	while thread_status != 3:
+		match thread_status:
+			0, 2:
+				return
+			_:
+				pass
+		await get_tree().process_frame
+		thread_status = ResourceLoader.load_threaded_get_status(scene_file)
+	
