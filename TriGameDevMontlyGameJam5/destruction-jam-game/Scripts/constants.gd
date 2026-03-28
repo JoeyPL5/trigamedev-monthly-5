023f665e1eeb6d48aa45a971bfd45e048f8fae7a
Joseph Pierre-Louis <97973691+JoeyPL5@users.noreply.github.com>
2026-03-27 22:42:33 -0400

    basic main menu

--- TriGameDevMontlyGameJam5/destruction-jam-game/Scripts/constants.gd
+++ TriGameDevMontlyGameJam5/destruction-jam-game/Scripts/constants.gd
@@ -19,6 +19,15 @@ const FLASH_WHITE : Color = Color(2.0, 2.0, 2.0)
 const BLACK : Color = Color("#000000")
 const INVIS : Color = Color("#ffffff00")
 
+# -- COLOR PALETTES
+# - ICYWITCH
+const DARK_GREEN : Color = Color("#313638")
+const DARK_BLUE : Color = Color("#32535f")
+const BLUE : Color = Color("#0a777a")
+const GREEN : Color = Color("#4aa881")
+const LIGHT_BLUE : Color = Color("#73efe8")
+const YELLOW : Color = Color("#ecf3b0")
+
 # BBCODE
 const WAVE_TEMPLATE : String = "[wave amp=20 freq=5 connected=0]%s[/wave]"
 const WAVE2_TEMPLATE : String = "[wave amp=10 freq=5 connected=0]%s[/wave]"
