1670b9d6f65610c031673080a71e602bcc9046f6
Vanessa Le <vle@bighatbio.com>
2026-03-28 16:38:37 -0400

    small fix

--- TriGameDevMontlyGameJam5/destruction-jam-game/Scripts/demo_level.gd
+++ TriGameDevMontlyGameJam5/destruction-jam-game/Scripts/demo_level.gd
@@ -84,11 +84,19 @@ func _patrol_loop() -> void:
 		$Owner.play_patrol_animation()
 		is_owner_patrolling = true
 		
-		var distance: float = patrol_point_a.distance_to(patrol_point_b)
+		# Pick a random point along the patrol path to turn around at
+		var turn_ratio: float = randf_range(0.3, 1.0)  # at least 30% of the way
+		var turnaround_point: Vector2 = patrol_point_a.lerp(patrol_point_b, turn_ratio)
+		
+		var distance: float = $Owner.global_position.distance_to(turnaround_point)
 		var duration: float = distance / patrol_speed
-
-		await $Owner.move_to(patrol_point_b, duration)
-		await $Owner.move_to(patrol_point_a, duration)
+		
+		await $Owner.move_to(turnaround_point, duration)
+		
+		var return_distance: float = turnaround_point.distance_to(patrol_point_a)
+		var return_duration: float = return_distance / patrol_speed
+		
+		await $Owner.move_to(patrol_point_a, return_duration)
 		
 		$Owner.stop_patrol()
 		is_owner_patrolling = false
